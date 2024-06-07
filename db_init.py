d3c46bc89ef3566e07d84567e7b008405f715adc
YelkuriRaghavendra <125492599+YelkuriRaghavendra@users.noreply.github.com>
2024-06-07 12:33:46 +0530

    Update db_init.py

--- db_init.py
+++ db_init.py
@@ -9,7 +9,7 @@ def get_connection():
         dbname="postgres",
         user="postgres",
         password="postgres",
-        host= "ec2-34-235-167-47.compute-1.amazonaws.com",
+        host= "postgres",
         port="5432"
     )
     return conn
